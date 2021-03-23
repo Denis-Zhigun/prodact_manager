8513c37a67d58b62a34ddc714388d0967d69785a
Denis Zhigun <zhigundv@gmail.com>
2021-03-23 22:40:35 +0300

    update project

--- src/main/java/ru/netology/domain/Book.java
+++ src/main/java/ru/netology/domain/Book.java
@@ -1,10 +1,20 @@
 package ru.netology.domain;
 
 public class Book extends Product {
+    private String name;
     private String author;
-
     public Book(int id, String name, int price) {
         super(id, name, price);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getAuthor() {
@@ -14,10 +24,10 @@ public class Book extends Product {
     public void setAuthor(String author) {
         this.author = author;
     }
-    
-    public Book(int id, String name, int price, String author) {
+
+    public Book(int id, String name, int price, String name1, String author) {
         super(id, name, price);
+        this.name = name1;
         this.author = author;
     }
 }
-
--- src/main/java/ru/netology/domain/Product.java
+++ src/main/java/ru/netology/domain/Product.java
@@ -1,7 +1,6 @@
 package ru.netology.domain;
 
 public class Product {
-
         private int id;
         private String name;
         private int price;
@@ -14,27 +13,21 @@ public class Product {
                 this.name = name;
                 this.price = price;
         }
-
         public int getId() {
                 return id;
         }
-
         public void setId(int id) {
                 this.id = id;
         }
-
         public String getName() {
                 return name;
         }
-
         public void setName(String name) {
                 this.name = name;
         }
-
         public int getPrice() {
                 return price;
         }
-
         public void setPrice(int price) {
                 this.price = price;
         }
